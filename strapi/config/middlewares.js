b7f1e82174a9a67808d4f6d3085941e46b053002
Angel Rios <angelrip05@gmail.com>
2026-05-31 01:28:50 +0200

    feat: agregar estructura completa de Strapi con 6 content types y relaciones

---
+++ strapi/config/middlewares.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = [
+  'strapi::errors', 'strapi::security', 'strapi::cors',
+  'strapi::poweredBy', 'strapi::logger', 'strapi::query',
+  'strapi::body', 'strapi::session', 'strapi::favicon', 'strapi::public',
+];
